e4b30a65e6b1c6287df43bf6a532bebe9398dec6
Alkagar <alkagar@gmail.com>
2012-12-13 15:50:23 +0100

    add css sprites for icons in task list

--- system/views/taskType/index.php
+++ system/views/taskType/index.php
@@ -1,21 +1,35 @@
+<?php
+    $tt = '';
+?>
+
 <h1>types lists</h1>
+
 
 <?php foreach($types as $taskType) : ?>
 <div>
     <?php 
-        $linkEdit = CHtml::link('edit', array('/taskType/edit', 'id' => $taskType->id)); 
-        $linkDelete = CHtml::link('x', array('/taskType/delete', 'id' => $taskType->id), array( 'confirm' => 'Are you sure?')); 
-        $linkAdd = CHtml::link('add attribute', array('/typeAttribute/add', 'id' => $taskType->id), array()); 
+        $linkEdit = CHtml::link('', array('/taskType/edit', 'id' => $taskType->id), array('class' => 'icon ico-settings')); 
+        $linkDelete = CHtml::link('', array('/taskType/delete', 'id' => $taskType->id), array( 'class' => 'icon ico-delete', 'confirm' => 'Are you sure?')); 
+        $linkAdd = CHtml::link('', array('/typeAttribute/add', 'id' => $taskType->id), array('class' => 'icon ico-round-plus', 'alt' => 'Add attribute')); 
+
+        $taskName = CHtml::tag('td', array(), $taskType->name);
+        $taskMenu = CHtml::tag('td', array(), $linkEdit . $linkDelete . $linkAdd);
+        $taskDesc = CHtml::tag('p', array(), $taskType->description);
+
+        $tr = CHtml::tag('tr', array(), $taskName . $taskMenu);
+        $tt .= $tr;
+        $tr = CHtml::tag('tr', array('colspan' => 2), $taskDesc);
+        $tt .= $tr;
     ?>
-    <strong>
-        <?php echo $taskType->name, ' - ', $taskType->description, ' - ', ($taskType->required == 1 ? 'wymagany - ' : '' ), $linkEdit, ' - ', $linkDelete, ' - ', $linkAdd; ?>
-    </strong>
     <?php foreach($taskType->typeAttributes as $typeAttr) : ?>
     <?php
-        $linkDelete = CHtml::link('x', array('/typeAttribute/delete', 'id' => $typeAttr->id), array( 'confirm' => 'Are you sure?')); 
-        $linkEdit = CHtml::link('edit', array('/typeAttribute/edit', 'id' => $typeAttr->id), array()); 
-        echo CHtml::tag('div', array('style' => 'padding-left:10px;'), $typeAttr->name . ' (' . $typeAttr->description . ') ' . $linkDelete . ' - ' . $linkEdit);
+        $linkDelete = CHtml::link('', array('/typeAttribute/delete', 'id' => $typeAttr->id), array( 'confirm' => 'Are you sure?', 'class' => 'icon ico-delete')); 
+        $linkEdit = CHtml::link('', array('/typeAttribute/edit', 'id' => $typeAttr->id), array('class' => 'icon ico-settings')); 
+        echo CHtml::tag('div', array('style' => 'padding-left:10px;'), $typeAttr->name . ' (' . $typeAttr->description . ') ' . $linkDelete . ' ' . $linkEdit);
     ?>
     <?php endforeach; ?>
 </div>
 <?php endforeach; ?>
+<?php 
+    echo $tt;
+?>
