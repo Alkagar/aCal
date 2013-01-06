25f2c6bf75efb9ede2a9eb8efe177b82f205d716
Jakub Mrowiec <alkagar@auriga.(none)>
2013-01-06 12:57:22 +0100

    add auriga config

---
+++ system/config/auriga.php
@@ -0,0 +1,11 @@
+<?php
+    return array(
+        'basePath'=>dirname(__FILE__).DIRECTORY_SEPARATOR.'..',
+        'components'=>array(
+            'db'=>array(
+                'connectionString' => 'sqlite:/home/alkagar/repos/sqlite/acalendar.db',
+            ),
+            'request' => array(
+               ),
+        ),
+    );
